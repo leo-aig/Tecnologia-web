f51fdcbf00c1219b2d5ce03f061402c5e7888698
Jhoel17 <vasquezjhoel578@gmail.com>
2026-02-27 10:40:55 -0400

    cambios main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from config.conexionDB import app
-from routes import cita, mascota, persona, usuario
+from routes import cita, mascota, persona, usuario, veterinario, control_tratamiento, tratamiento, historial_clinico
 
 
 app.include_router(persona.router, prefix="/personas")
 app.include_router(usuario.router, prefix="/usuarios")
 app.include_router(mascota.router, prefix="/mascotas")
 app.include_router(cita.router, prefix="/citas")
+app.include_router(veterinario.router, prefix="/veterinario")
+app.include_router(historial_clinico.router, prefix="/historial")
+app.include_router(tratamiento.router, prefix="/tratamiento")
+app.include_router(control_tratamiento.router, prefix="/control")
 
 
 @app.get("/")
