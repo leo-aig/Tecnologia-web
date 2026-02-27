910e3c490a8b22230f1cbcca0ba4d067fa02aab8
Leandro <leandroleyesclavijo@gmail.com>
2026-02-27 10:40:02 -0400

    ingreso del main

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from config.conexionDB import app
-from routes import cita, mascota, persona, usuario
+from routes import cita, control_tratamiento, historial_clinico, mascota, persona, tratamiento, usuario, veterinario
 
 
 app.include_router(persona.router, prefix="/personas")
 app.include_router(usuario.router, prefix="/usuarios")
 app.include_router(mascota.router, prefix="/mascotas")
 app.include_router(cita.router, prefix="/citas")
+app.include_router(veterinario.router, prefix="/veterinarios")
+app.include_router(historial_clinico.router, prefix="/historial")
+app.include_router(control_tratamiento.router, prefix="/control")
+app.include_router(tratamiento.router, prefix="/tratamientos")
 
 
 @app.get("/")
